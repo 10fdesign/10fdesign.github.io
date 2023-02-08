ab03c67121f9758914ba8f6d59736ec4c5fe9446
Oscar Heller <oheller25@gmail.com>
2023-02-08 12:55:56 -0500

    Tweaked header and blog CSS.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,60 @@
-function initializeSlider(){var l=0,o=(new Date,$(".card:not(.hidden-card)"));for(let e=0;e<o.length;e++)l+=1,$(o[e]).delay(150*l).queue(function(){$(this).addClass("card-visible");var l=new Date;l});$(".testimonials-slider").slick({fade:!1,autoplay:!0,dots:!0,speed:200,autoplaySpeed:5e3,cssEase:"linear",prevArrow:'<button type="button" class="slick-prev"><i class="icofont-simple-left"></i></button>',nextArrow:'<button type="button" class="slick-next"><i class="icofont-simple-right"></i></button>'})}$.fn.disableScroll=function(){window.oldScrollPos=$(window).scrollTop(),$(window).on("scroll.scrolldisabler",function(l){$(window).scrollTop(window.oldScrollPos),l.preventDefault()})},$.fn.enableScroll=function(){$(window).off("scroll.scrolldisabler")},$(document).ready(function(){$(window).scroll(function(){$("body").hasClass("menu-open")||($(this).scrollTop()>40?$("body").addClass("scrolled"):$("body").removeClass("scrolled"))}),$("label.menu-toggle.menu-toggle-open").click(function(){$("body").addClass("menu-open"),$("body").disableScroll()}),$("label.menu-toggle.menu-toggle-close").click(function(){$("body").removeClass("menu-open"),$("body").enableScroll()}),initializeSlider()});
+$.fn.disableScroll = function() {
+    window.oldScrollPos = $(window).scrollTop();
+
+    $(window).on('scroll.scrolldisabler',function ( event ) {
+       $(window).scrollTop( window.oldScrollPos );
+       event.preventDefault();
+    });
+};
+
+$.fn.enableScroll = function() {
+    $(window).off('scroll.scrolldisabler');
+};
+
+$(document).ready( function () {
+  $(window).scroll( function (e) {
+    if (!$("body").hasClass("menu-open")) {
+      if ( $(this).scrollTop() > 40 ) {
+        $("body").addClass("scrolled");
+      } else {
+        $("body").removeClass("scrolled");
+      }
+    }
+  });
+
+  $('label.menu-toggle.menu-toggle-open').click(function() {
+    $("body").addClass("menu-open");
+    $("body").disableScroll();
+  });
+
+  $('label.menu-toggle.menu-toggle-close').click(function() {
+    $("body").removeClass("menu-open");
+    $("body").enableScroll();
+  });
+
+  initializeSlider();
+});
+
+function initializeSlider() {
+  var index = 0;
+  var last_time = new Date();
+  var cards = $('.card:not(.hidden-card)');
+  for ( let i = 0; i < cards.length; i++ ) {
+    index += 1;
+    $(cards[i]).delay( index * 150 ).queue( function () {
+      $(this).addClass( "card-visible" );
+      var new_time = new Date();
+      last_time = new_time;
+    });
+  }
+  $(".testimonials-slider").slick({
+    fade: false,
+    autoplay: true,
+    dots: true,
+    speed: 200,
+    autoplaySpeed: 5000,
+    cssEase: 'linear',
+    prevArrow: '<button type="button" class="slick-prev"><i class="icofont-simple-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="icofont-simple-right"></i></button>',
+  });
+}
